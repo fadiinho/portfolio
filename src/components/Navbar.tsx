619558a2be367f25b36895f8c311bdf4071ed0b6
fadiinho <ffadiinho@gmail.com>
2022-11-20 16:06:14 -0300

    feat: close menu on link click

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,6 +23,7 @@ export default function Navbar() {
 
       <div 
         className={`${menuOpen ? "" : "translate-x-full opacity-0"} w-screen sm:w-auto py-8 sm:p-0 sm:translate-y-0 sm:translate-x-0 transition-all translate-y-14 inset-0 bottom-auto absolute sm:static bg-background sm:flex flex-col justify-between`}
+        onClick={() => setMenuOpen(false)}
       >
         <ul 
           className="w-full sm:w-fit flex flex-col sm:flex-row sm:justify-end sm:items-end sm:gap-8">
